701a7d0000a53a9ef8b0e74481df4a896888ff4c
吴中贤 <wing4123@163.com>
2025-03-24 18:25:20 +0800

    Update main.ts

--- src-cli/main.ts
+++ src-cli/main.ts
@@ -1,8 +1,8 @@
-import * as canvas from "canvas";
-import * as fs from "fs";
-import * as minimist from "minimist";
-import * as path from "path";
-import * as process from "process";
+import canvas from "canvas";
+import fs from "fs";
+import minimist from "minimist";
+import path from "path";
+import process from "process";
 import { ColorReducer } from "../src/colorreductionmanagement";
 import { RGB } from "../src/common";
 import { FacetBorderSegmenter } from "../src/facetBorderSegmenter";
