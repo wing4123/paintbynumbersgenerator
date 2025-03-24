625fa866ecac06370fd746623f0ce9e5c81383aa
吴中贤 <wing4123@163.com>
2025-03-24 18:27:56 +0800

    Update common.ts

--- src/common.ts
+++ src/common.ts
@@ -7,9 +7,9 @@ export interface IMap<T> {
 
 export async function delay(ms: number) {
     if (typeof window !== "undefined") {
-        return new Promise((exec) => (<any> window).setTimeout(exec, ms));
+        return new Promise((exec) => window.setTimeout(exec, ms));
     } else {
-        return new Promise((exec) => exec());
+        return new Promise<void>((exec) => exec()); // 确保传入 void
     }
 }
 
